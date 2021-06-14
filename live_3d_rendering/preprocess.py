00b9f712c40ed7edc2012438db14bda6af2437cc
Thomas Breydo <tbreydo@gmail.com>
2021-06-13 22:48:50 -0400

    Implement image preprocessing

---
+++ live_3d_rendering/preprocess.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageDraw
+import math
+
+
+def _distance_to_white(image, thresh=180):
+    """Dist from center to white region"""
+    cx = image.size[0] // 2
+    cy = image.size[1] // 2
+    im = image.load()
+    lo = 0
+    hi = cx
+    while lo < hi:
+        mid = (lo + hi) // 2
+        if im[mid, 0] > thresh:  # white
+            lo = mid + 1
+        else:  # not white
+            hi = mid
+    return math.sqrt(cy ** 2 + (cx - lo) ** 2)
+
+
+def blacken_white_pixels(image, radius):
+    """Blacken all pixels outside of the circular region of interest"""
+    cx = image.size[0] // 2
+    cy = image.size[1] // 2
+    background = Image.new(image.mode, image.size, 0)
+    mask = Image.new("L", image.size, 0)
+    ImageDraw.Draw(mask).ellipse(
+        (
+            (cx - radius, cy - radius),
+            (cx + radius, cy + radius),
+        ),
+        fill=255,
+    )
+    return Image.composite(image, background, mask)
+
+
+def preprocess(image, white_treshhold=180):
+    if image.mode != "L":
+        image = image.convert("L")
+    image = blacken_white_pixels(image, _distance_to_white(image, white_treshhold) - 10)
+    image = image.point(lambda p: p * (p < white_treshhold))
+    return image
